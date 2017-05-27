62abde11d2730af73b3bd2dd76c329117eb6e434
Robert Hoppe <rh@address-base.de>
2017-05-27 15:44:51 +0200

    [TASK] Push first version for testing

--- ellipsis.sh
+++ ellipsis.sh
@@ -27,20 +27,43 @@ git-configured() {
 }
 
 pkg.install() {
-    # Backup existing configuration
-    fs.backup ~/.vimrc
-    fs.backup ~/.wgetrc
-    fs.backup ~/.screenrc
-    fs.backup ~/.zsh
+    # call custom helper for different topics
+    git_setup
+    font_setup
+    zsh_setup
+}
 
+common_setup() {
+    # create vim directory
+    mkdir -p ~/.vim/colors
+
+    # copy theme
+    cp "$PKG_PATH/files/vim/solarized.vim" ~/.vim/colors
+
+    # copy screen stats
+    cp "$PKG_PATH/files/screen/screen-stats.awk" /usr/local/bin/screen-stats.awk
+    chmod + /usr/local/bin/screen-stats.awk
+}
+
+git_setup() {
     git config --global user.name "Robert Hoppe"
     git config --global user.email "robert.hoppe@nodemash.com"
     git config --global github.user "roberth1988"
+}
 
+font_setup() {
     # install fonts
     git clone https://github.com/powerline/fonts.git /tmp/powerline-fonts
     cd /tmp/powerline-fonts; $1 ./install.sh
     rm -fr /tmp/powerline-fonts
+}
 
+zsh_setup() {
+    # get current version of prezto
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
 }
